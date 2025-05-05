a927b8fbbdbe45e17cbd83273e3d882d7cafad0e
MightyGud <trimanh0104@gmail.com>
2025-05-05 18:26:04 +1000

    update script.js

--- Assignment 2/script.js
+++ Assignment 2/script.js
@@ -1,32 +1,32 @@
-const topHeading = document.querySelector("#top-heading")
-console.log(topHeading)
+const topHeading = document.querySelector("#top-heading");
+console.log(topHeading);
 
 let myNewHeading = "Music is playing";
 
-const erokiaSound = document.querySelector("#erokia_audio")
-console.log(erokiaSound)
+const erokiaSound = document.querySelector("#erokia_audio");
+console.log(erokiaSound);
 
 // play button
-const playButton = document.querySelector("#play-button")
-console.log(playButton)
+const playButton = document.querySelector("#play-button");
+console.log(playButton);
 
-playButton.addEventListener("click",playAudio)
+playButton.addEventListener("click", playAudio);
 
 function playAudio() {
-erokiaSound.play();
+  erokiaSound.play();
 }
 
 erokiaSound.loop = true;
 //-----
 
 // stop button
-const pauseButton = document.querySelector("#pause-button")
-console.log(pauseButton)
+const pauseButton = document.querySelector("#pause-button");
+console.log(pauseButton);
 
-pauseButton.addEventListener("click",pauseAudio);
+pauseButton.addEventListener("click", pauseAudio);
 
 function pauseAudio() {
-erokiaSound.pause();
+  erokiaSound.pause();
 }
 //-----
 
@@ -47,11 +47,13 @@ const muteUnmuteImg = document.querySelector("#mute-unmute-img");
 console.log(muteUnmuteImg);
 
 function toggleAudio() {
-    if (erokiaSound.muted){ 
-        erokiaSound.muted = false;
-        muteUnmuteImg.src ="https://img.icons8.com/ios-glyphs/30/high-volume--v2.png"
-    } else {
-        erokiaSound.muted = true;
-        muteUnmuteButton.src =  "https://img.icons8.com/ios-glyphs/30/no-audio--v1.png"
-    }
-    }
+  if (erokiaSound.muted) {
+    erokiaSound.muted = false;
+    muteUnmuteImg.src =
+      "https://img.icons8.com/ios-glyphs/30/high-volume--v2.png";
+  } else {
+    erokiaSound.muted = true;
+    muteUnmuteButton.src =
+      "https://img.icons8.com/ios-glyphs/30/no-audio--v1.png";
+  }
+}
